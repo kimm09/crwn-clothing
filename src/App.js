1e258ed8fa05441adfb81a1b5c311be3da080062
Kim <goverkim@msn.com>
2020-07-01 12:08:03 -0400

    updating code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
+
+
+
 
 function App() {
   return (
-<div > 
-   <HomePage/>
-</div>
-  ) ;
+    <div>
+    <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
